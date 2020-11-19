005017fa41744f852643aaf913e47ee0cce783a5
Dale Woodard <dale@wingedw.com>
2020-11-19 13:20:26 -0800

    Extracted variables and added to env file

--- api.py
+++ api.py
@@ -2,32 +2,37 @@ from flask import Flask
 from flask_restful import Resource, Api
 #from flask_cors import CORS
 import requests
- 
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
 app = Flask(__name__)
 api = Api(app)
-#CORS(app) ## To allow direct AJAX calls
+#CORS(app) ## To a low direct AJAX calls
  
 @app.route('/year', methods=['GET'])
 def Get():
-    uri='https://webapi.nhtsa.gov/api/SafetyRatings?format=json'
+    uri= environ.get('YEARS')
     r = requests.get(uri)
     return r.json()
 
 @app.route('/make/<year>', methods=['GET'])
 def GetMakes(year):
-    uri='https://webapi.nhtsa.gov/api/SafetyRatings/modelyear/{0}?format=json'
+    uri= environ.get('MAKES')
     r = requests.get(uri.format(year))
     return r.json()
 
 @app.route('/model/<year>/<make>', methods=['GET'])
 def GetModel(year,make):
-    uri='https://webapi.nhtsa.gov/api/SafetyRatings/modelyear/{0}/make/{1}?format=json'
+    uri = environ.get('MODELS')
     r = requests.get(uri.format(year,make))
     return r.json()
 
 @app.route('/report/<year>/<make>/<model>', methods=['GET'])
 def GetReport(year,make,model):
-    uri='https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/{0}/make/{1}/model/{2}?format=json'
+    uri = environ.get('REPORT')
     r = requests.get(uri.format(year,make,model))
     return r.json()
 
